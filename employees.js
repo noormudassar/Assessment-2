f4f12144e68056c3c81744777e0ab90352b09e80
Noor Mudassar <noormudassar97@gmail.com>
2023-07-02 18:33:14 -0400

    solved employees.js

--- employees.js
+++ employees.js
@@ -21,7 +21,16 @@
 
 //CODE HERE
 
+class Employee {
+  constructor(name, shifts) {
+    this.name = name;
+    this.shifts = shifts;
+  }
 
+  getSchedule() {
+    console.log(`${this.name} works on ${this.shifts}`);
+  }
+}
 
 /*
     Create a new instance of your class.
@@ -35,6 +44,10 @@
 
 //CODE HERE
 
+empOne = new Employee("Jess", "weekday mornings, weekday afternoons");
+
+// console.log(empOne);
+
 /*
     Call the `getSchedule` method on the
     `empOne` object.
@@ -42,6 +55,7 @@
 
 //CODE HERE
 
+empOne.getSchedule();
 
 /*
     Make a copy of the empOne object
@@ -57,7 +71,8 @@
 
 //CODE HERE
 
-
+const empTwo = { ...empOne };
+empTwo.name = "Nick";
 
 //////////////////PROBLEM 2////////////////////
 /*  
@@ -84,7 +99,20 @@
 
 //CODE HERE
 
+class Manager extends Employee {
+  constructor(name, shifts, employees) {
+    super(name, shifts);
+    this.employees = employees;
+  }
 
+  getEmployees() {
+    console.log(`${this.name} manages ${this.employees}`);
+  }
+
+  addEmployee(emp) {
+    this.employees.push(emp);
+  }
+}
 
 /*
     Create a new instance of your class.
@@ -99,6 +127,11 @@
 
 //CODE HERE
 
+const manager = new Manager(
+  " Winston",
+  "weekday mornings, weekday afternoons",
+  ["Cece", "Schmidt"]
+);
 
 /*
     Call the `getEmployees` method on the
@@ -107,13 +140,17 @@
 
 //CODE HERE
 
+manager.getEmployees();
+
 /*
     Call the `addEmployee` method on the 
     `manager` object passing in the string 
     'Coach' or whatever name you'd like.
 */
 
-//CODE HERE 
+//CODE HERE
+
+manager.addEmployee("Coach");
 
 /*
     Call the `getEmployees` method on the
@@ -122,3 +159,5 @@
 */
 
 //CODE HERE
+
+manager.getEmployees();
